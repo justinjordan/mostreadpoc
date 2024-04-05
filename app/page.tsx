ac5004b8d8d6ff4525a92ae272876982ae90993f
Justin Jordan <justinjordan831@gmail.com>
2024-04-05 13:39:28 -0500

    Error message for none found

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,11 @@ export default async function Home() {
         <section className="p-4" key={section.title}>
           <h2 className="text-large mb-4">{section.title}</h2>
           <article className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {section.posts.length === 0 && (
+              <div className="text-center text-gray-500">
+                No articles found.
+              </div>
+            )}
             {section.posts.map((post) => (
               <Card key={post.id} title={post.title}>
                 <CardHeader>
